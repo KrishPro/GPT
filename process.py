7615e9690cd442d4585dd4f75c3b4e8f7c9dfe31
Krish Baisoya <krishbaisoya.it2020@gmail.com>
2022-11-17 03:31:07 +0530

    Added a function to append num_sentences at the start of file, so that the dataset knows how much datapoints to expect

--- process.py
+++ process.py
@@ -9,6 +9,7 @@ from typing import Callable, List
 from unidecode import unidecode
 from tokenizers import Tokenizer
 import multiprocessing as ml
+import time
 import math
 import os
 
@@ -97,10 +98,23 @@ def process(input_path: str, output_path: str, vocab_path: str, chunk_size=2**10
                         output_file.write("\n".join([" ".join(map(str, tokens)) for tokens in seqs_tokens]) + ("" if i == (num_texts-1) else "\n"))
                     texts = []
 
+def append_num_sentences(file_path: str, tmp_dir: str = '.'):
+    num_sentences = count_lines(file_path, guessed_num_sentences=9085021)
+    tmp_path = os.path.join(tmp_dir, str(time.time()))
+
+    with open(file_path) as input_file:
+        with open(tmp_path, 'w') as output_file:
+            output_file.write(f"{num_sentences}\n")
+            for t in tqdm(input_file, total=num_sentences):
+                output_file.write(t)
+
+    os.remove(file_path)
+    os.rename(tmp_path, file_path)
+
+
+
 if __name__ == "__main__":
     # apply("<input-file>", "<output-file>", unidecode)
     # process("<input-file>", "<output-file>", vocab_path='<vocab-path>')
-  
+    # append_num_sentences("<input-file>")
     pass
-
-            
